98ea21f689f067f1d7a9745e6eaa48b9cd671694
luka5 <lh@mikanda.de>
2013-07-05 20:38:36 +0200

    array keys nolonger need to be continuous

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var object = require('object'),
     each = require('each'),
     value = require('value'),
     query = require('query'),
+    attr = require('attr'),
     type = require('type');
 
 /**
@@ -31,18 +32,43 @@ function updateAttr (key, meta) {
       el,
       part,
       obj,
-      i = 0;
+      i = 0,
+      keyParts = key.split('.');
 
   if (key === 'type' || key === 'required')
     return;
 
   if (type(meta) === 'array' && meta.length >= 1) {
-    while (query('[name^="' + key + '.' + i + '"]', this.form) !== null) {
-      each(meta[0], function (k, v) {
-        updateAttr(key + '.' + i + '.' + k, v);
-      });
+    this.model[keyParts[0]]([]);
+    var amount = 0;
+    var tmp = {};
+    var allElements = query.all('[name^="' + key + '."]', this.form);
+    each(allElements, function (el) {
+      var name,
+          nameParts;
+      name = attr(el).get('name');
+      nameParts = name.split('.');
+      if (nameParts.length >= 1 && nameParts[1]*1 == nameParts[1])
+        tmp[nameParts[0] + '.' + nameParts[1]] = true;
+    });
+    amount = object.length(tmp);
+    var count = 0;
+    while (amount > count) {
+      if (query('[name^="' + key + '.' + i + '"]', this.form) !== null) {
+        each(meta[0], function (k, v) {
+          updateAttr(key + '.' + i + '.' + k, v);
+        });
+        count++;
+      }
       i++;
     }
+    //rearrange array
+    var newArray = this.model[keyParts[0]]();
+    obj = [];
+    for(var index in newArray)
+      if (newArray[index])
+        obj.push(newArray[index]);
+    this.model[keyParts[0]](obj);
     return;
   }
 
@@ -58,21 +84,20 @@ function updateAttr (key, meta) {
     return;
   val = value(el);
 
-  key = key.split('.');
-  obj = this.model[key[0]]();
+  obj = this.model[keyParts[0]]();
   if (obj === undefined)
     obj = {};
-  if (key.length === 1) {
+  if (keyParts.length === 1) {
     obj = val;
   } else {
     part = obj;
-    for (var i = 1; i < key.length-1; i++) {
-      if (part[key[i]] === undefined)
-        part[key[i]] = {};
-      part = part[key[i]];
+    for (var i = 1; i < keyParts.length-1; i++) {
+      if (part[keyParts[i]] === undefined)
+        part[keyParts[i]] = {};
+      part = part[keyParts[i]];
     }
-    part[key[key.length-1]] = val;
+    part[keyParts[keyParts.length-1]] = val;
   }
-  this.model[key[0]](obj);
+  this.model[keyParts[0]](obj);
 
 };
