7de2be984890cbb96aa912c6684c5bb891823424
luka5 <lh@mikanda.de>
2013-07-08 12:07:58 +0200

    using json-schema now

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var object = require('object'),
     value = require('value'),
     query = require('query'),
     attr = require('attr'),
-    type = require('type');
+    type = require('type'),
+    validator = require('amanda')('json');
 
 /**
  * Module exports.
@@ -16,33 +17,57 @@ var object = require('object'),
 
 module.exports = syncModel;
 
-function syncModel (form, model) {
+function syncModel (form, model, opt) {
   this.form = form;
   if (type(model) === 'function') {
     model = new model();
   }
   this.model = model;
   this.attrs = model.model.attrs;
-  each(attrs, updateAttr);
+  each(attrs, function (key, meta) {
+    var data;
+    search(key, meta);
+
+    if (!opt || opt.validate === true) {
+      data = this.model[key]();
+      validator.validate(data, meta, function(error) {
+        if (error)
+          console.log(key + ' validator error: ', error, error.getProperties(), error.getMessages());
+      });
+    }
+  });
   return model;
 };
 
-function updateAttr (key, meta) {
+function search (key, meta) {
   var val,
       el,
       part,
       obj,
       i = 0,
-      keyParts = key.split('.');
+      amount = 0,
+      count = 0,
+      tmp = {},
+      allElements,
+      index;
 
-  if (key === 'type' || key === 'required')
-    return;
+  if (!meta || type(meta) === 'string' || meta.type === 'boolean' || meta.type === 'integer' || meta.type === 'number' || meta.type === 'string' || meta.type === 'null') {
+    el = query('[name="'+key+'"]', this.form);
+    if (!el)
+      return;
+    val = value(el);
+    if (meta.type === 'integer' || meta.type === 'number')
+      val = val*1;
+    updateValue(key, val);
 
-  if (type(meta) === 'array' && meta.length >= 1) {
-    this.model[keyParts[0]]([]);
-    var amount = 0;
-    var tmp = {};
-    var allElements = query.all('[name^="' + key + '."]', this.form);
+  } else if (meta.type === 'object') {
+    each(meta.properties, function (k, v) {
+      search(key + '.' + k, v);
+    });
+
+  } else if (meta.type === 'array') {
+    updateValue(key, []);
+    allElements = query.all('[name^="' + key + '."]', this.form);
     each(allElements, function (el) {
       var name,
           nameParts;
@@ -52,38 +77,31 @@ function updateAttr (key, meta) {
         tmp[nameParts[0] + '.' + nameParts[1]] = true;
     });
     amount = object.length(tmp);
-    var count = 0;
+
     while (amount > count) {
       if (query('[name^="' + key + '.' + i + '"]', this.form) !== null) {
-        each(meta[0], function (k, v) {
-          updateAttr(key + '.' + i + '.' + k, v);
-        });
+        search(key + '.' + i, meta.items);
         count++;
       }
       i++;
     }
-    //rearrange array
-    var newArray = this.model[keyParts[0]]();
+
+    //rearrange array, make it continuous
+    tmp = this.model[key.split('.')[0]]();
     obj = [];
-    for(var index in newArray)
-      if (newArray[index])
-        obj.push(newArray[index]);
-    this.model[keyParts[0]](obj);
+    for(index in tmp)
+      if (tmp[index])
+        obj.push(tmp[index]);
+    this.model[key.split('.')[0]](obj);
     return;
   }
+};
 
-  if (type(meta) === 'object' && !object.isEmpty(meta)) {
-    each(meta, function (k, v) {
-      updateAttr(key + '.' + k, v);
-    });
-    return;
-  }
-
-  el = query('[name="'+key+'"]', this.form);
-  if (!el)
-    return;
-  val = value(el);
-
+function updateValue (key, val) {
+  var obj,
+      part,
+      i,
+      keyParts = key.split('.');
   obj = this.model[keyParts[0]]();
   if (obj === undefined)
     obj = {};
@@ -91,7 +109,7 @@ function updateAttr (key, meta) {
     obj = val;
   } else {
     part = obj;
-    for (var i = 1; i < keyParts.length-1; i++) {
+    for (i = 1; i < keyParts.length-1; i++) {
       if (part[keyParts[i]] === undefined)
         part[keyParts[i]] = {};
       part = part[keyParts[i]];
@@ -99,5 +117,4 @@ function updateAttr (key, meta) {
     part[keyParts[keyParts.length-1]] = val;
   }
   this.model[keyParts[0]](obj);
-
 };
