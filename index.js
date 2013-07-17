418192fa0ff932fbbb2a2110cc8ce7c2af2de236
luka5 <lh@mikanda.de>
2013-07-17 18:18:59 +0200

    no validation + array has to be continuous

--- index.js
+++ index.js
@@ -8,81 +8,38 @@ var object = require('object'),
     value = require('value'),
     query = require('query'),
     attr = require('attr'),
-    type = require('type'),
-    Batch = require('batch'),
-    validator = require('amanda')('json');
+    type = require('type');
 
 /**
  * Module exports.
  */
 
-module.exports = function (form, model, callback, callbackScope, opt) {
-  return new SyncModel(form, model, callback, callbackScope, opt);
-};
+module.exports = SyncModel;
 
-function SyncModel (form, model, callback, callbackScope, opt) {
-  var batch = new Batch,
-      self = this;
+function SyncModel (form, model, callback, callbackScope) {
+  if (!(this instanceof SyncModel))
+    return new SyncModel(form, model, callback, callbackScope);
+
+  var self = this;
   this.form = form;
-  if (type(model) === 'function') {
-    model = new model();
-  }
   this.model = model;
-  this.attrs = model.model.attrs;
-  each(this.attrs, function (key, meta) {
-    var data;
-    self.search(key, meta);
+  if (type(model) === 'function')
+    this.model = model = new model();
 
-    if (!opt || opt.validate === true) {
-      if (!opt.amanda)
-        opt.amanda = {singleError: false};
-      data = self.model[key]();
-      batch.push(function (done) {
-        validator.validate(data, meta, opt.amanda, function (err) {
-          var errors = [],
-              i;
-          if (err && err.length) {
-            for (i = 0; i < err.length; i++) {
-              if (err[i].property === undefined || err[i].property == '')
-                err[i].property = key;
-              else
-                err[i].property = key + '.' + err[i].property;
-              errors.push(err[i]);
-            }
-          }
-          done(null, errors);
-        });
-      });
-    }
-  });
-  console.log('batch register end');
-  batch.end(function (_, _errors) {
-    var errors = [];
-    each(_errors, function (err, index) {
-      if (err.length > 0)
-        errors = errors.concat(err);
-    });
-    if (errors.length === 0)
-      errors = null;
-console.log('sync model end, apply callback with arguments:', errors);
-    callback.call(callbackScope, errors);
+  each(model.model.attrs, function (key, meta) {
+    self.search(key, meta);
   });
   return model;
 };
 SyncModel.prototype.search = function (key, meta) {
   var val,
       el,
-      part,
-      obj,
-      i = 0,
-      amount = 0,
-      count = 0,
-      tmp = {},
-      allElements,
-      index,
+      i,
       self = this;
 
-  if (!meta || type(meta) === 'string' || meta.type === 'boolean' || meta.type === 'integer' || meta.type === 'number' || meta.type === 'string' || meta.type === 'null') {
+  if (!meta
+      || type(meta) === 'string'
+      || meta.type.match(/^boolean|integer|number|string|null$/g) !== null) {
     el = query('[name="'+key+'"]', this.form);
     if (!el)
       return;
@@ -98,33 +55,11 @@ SyncModel.prototype.search = function (key, meta) {
 
   } else if (meta.type === 'array') {
     this.updateValue(key, []);
-    allElements = query.all('[name^="' + key + '."]', this.form);
-    each(allElements, function (el) {
-      var name,
-          nameParts;
-      name = attr(el).get('name');
-      nameParts = name.split('.');
-      if (nameParts.length >= 1 && nameParts[1]*1 == nameParts[1])
-        tmp[nameParts[0] + '.' + nameParts[1]] = true;
-    });
-    amount = object.length(tmp);
-
-    while (amount > count) {
-      if (query('[name^="' + key + '.' + i + '"]', this.form) !== null) {
-        self.search(key + '.' + i, meta.items);
-        count++;
-      }
+    i = 0;
+    while (query('[name^="' + key + '.' + i + '"]', this.form) !== null) {
+      self.search(key + '.' + i, meta.items);
       i++;
     }
-
-    //rearrange array, make it continuous
-    tmp = this.model[key.split('.')[0]]();
-    obj = [];
-    for(index in tmp)
-      if (tmp[index])
-        obj.push(tmp[index]);
-    this.model[key.split('.')[0]](obj);
-    return;
   }
 };
 SyncModel.prototype.updateValue = function (key, val) {
